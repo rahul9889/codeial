03c8377c2bf1be1d0b32e7cc84ac95964796555d
ryadav9889 <aka988919@gmail.com>
2023-05-15 01:28:03 +0530

    add controller and route

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home=function(req,res){
+    return res.end('<h1>hiii</h1>');
+}
+
+// module.exports.user = function(req,res){
+//     return res.end('<h1>Rahulll</h1>')
+// }
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+ const homeController = require('../controllers/home_controller');
+
+
+router.get('/',homeController.home);
+
+// router.get('/user',homeController.user);
+
+module.exports = router;
